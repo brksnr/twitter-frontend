92d1898f432086119aaa58c465f60ed943dcbcda
brksnr <berksener04@gmail.com>
2025-02-07 14:04:33 +0300

    sidebar için iconlar eklendi.

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,21 +1,26 @@
 export function Aside(){
     return (
         <>
-        <ul>
-            <li>Ana Sayfa</li>
-            <li>Keşfer</li>
-            <li>Bildirimler</li>
-            <li>Mesajlar</li>
-            <li>Grok</li>
-            <li>Yer İşaretleri</li>
-            <li>İşler</li>
-            <li>Topluluklar</li>
-            <li>Premium</li>
-            <li>Onaylı Kuruluşlar</li>
-            <li>Profil</li>
-            <li>Daha fazla</li>
-            <li>Gönderi yayınla</li>
+        <div className="bg-black">
+        <ul className="border text-white max-w-Aside items-end max-h-screen flex flex-col gap-10">
+            <div className="border flex flex-col gap-12 min-w-64">
+            <li><button className="border rounded-full w-40 h-12"><i class="fa-solid fa-x"></i> Ana Sayfa</button></li>
+            <li><button className="border"><i class="fa-solid fa-house"></i> Ana Sayfa</button></li>
+            <li><i class="fa-solid fa-magnifying-glass"></i>Keşfet</li>
+            <li><button><i class="fa-regular fa-bell"></i>Bildirimler</button></li>
+            <li><button><i class="fa-regular fa-envelope"></i>Mesajlar</button></li>
+            <li><button>Grok</button></li>
+            <li><button><i class="fa-regular fa-bookmark"></i>Yer İşaretleri</button></li>
+            <li><button><i class="fa-solid fa-briefcase"></i>İşler</button></li>
+            <li><button><i class="fa-solid fa-user-group"></i>Topluluklar</button></li>
+            <li><button><i class="fa-regular fa-circle-check"></i>Premium</button></li>
+            <li><button><i class="fa-solid fa-cloud-bolt"></i>Onaylı Kuruluşlar</button></li>
+            <li><button><i class="fa-regular fa-user"></i>Profil</button></li>
+            <li><button><i class="fa-regular fa-user"></i>Profil</button></li>
+            <li><button>Gönderi yayınla</button></li>
+            </div>
         </ul>
+        </div>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
  export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'Aside': '37rem',
+      }
+    },
   },
   plugins: [],
 }
