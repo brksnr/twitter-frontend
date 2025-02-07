ea413ae4f25ad994459ba05465fa69522d97ac0d
brksnr <berksener04@gmail.com>
2025-02-07 13:24:39 +0300

    Aside componenti oluşturuldu. Yazılar eklendi.

---
+++ src/components/Aside.jsx
@@ -0,0 +1,21 @@
+export function Aside(){
+    return (
+        <>
+        <ul>
+            <li>Ana Sayfa</li>
+            <li>Keşfer</li>
+            <li>Bildirimler</li>
+            <li>Mesajlar</li>
+            <li>Grok</li>
+            <li>Yer İşaretleri</li>
+            <li>İşler</li>
+            <li>Topluluklar</li>
+            <li>Premium</li>
+            <li>Onaylı Kuruluşlar</li>
+            <li>Profil</li>
+            <li>Daha fazla</li>
+            <li>Gönderi yayınla</li>
+        </ul>
+        </>
+    )
+}
