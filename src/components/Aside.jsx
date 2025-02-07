aa4906e75010cd07775ba6d1de561decf891d9da
brksnr <berksener04@gmail.com>
2025-02-07 15:38:07 +0300

    sidebar artık çözünürlüğe göre değişiyor.

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,23 +1,30 @@
 export function Aside(){
+        const button = "flex items-center gap-5 rounded-full w-fit pr-5 pl-2 h-12 hover:bg-darkgray text-xl font-medium"
     return (
         <>
         <div className="bg-black pt-2">
-        <ul className="text-white max-w-Aside items-end h-screen flex flex-col gap-10">
+        <ul className="text-white xl:max-w-Aside xl:items-end h-screen flex flex-col gap-10">
             <div className="flex flex-col gap-3 min-w-64">
-            <li><button className="flex items-center gap-5 rounded-full w-42 p-2 h-10 hover:bg-darkgray text-3xl font-bold"><i class="fa-solid fa-x"></i></button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-darkgray text-xl font-medium"><i class="fa-solid fa-house"></i> Ana Sayfa</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-solid fa-magnifying-glass"></i>Keşfet</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-regular fa-bell"></i>Bildirimler</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-regular fa-envelope"></i>Mesajlar</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-solid fa-hand-holding-heart"></i>Grok</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-regular fa-bookmark"></i>Yer İşaretleri</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-solid fa-briefcase"></i>İşler</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-solid fa-user-group"></i>Topluluklar</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-regular fa-circle-check"></i>Premium</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-solid fa-poo-storm"></i>Onaylı Kuruluşlar</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-regular fa-user"></i>Profil</button></li>
-            <li><button className="flex items-center gap-5 rounded-full w-42 pr-5 pl-2 h-12 hover:bg-gray-500 text-xl font-medium"><i class="fa-solid fa-ellipsis"></i>Daha Fazla</button></li>
-            <li><button className="bg-white text-black flex items-center justify-center gap-5 rounded-full w-full h-12 text-xl font-medium">Gönderi yayınla</button></li>
+            <button className="flex items-center gap-5 rounded-full w-fit p-2 h-10 hover:bg-darkgray text-3xl font-bold"><i class="fa-solid fa-x"></i></button>
+            <button className={button}>
+                <i class="fa-solid fa-house"></i>
+                <p className="hidden xl:flex">Anasayfa</p>
+            </button>
+            <button className={button}>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <p className="hidden xl:flex">Keşfet</p>
+            </button>
+            <button className={button}><i className="fa-regular fa-bell"></i><p className="hidden xl:flex">Bildirimler</p></button>
+            <button className={button}><i className="fa-regular fa-envelope"></i><p className="hidden xl:flex">Mesajlar</p></button>
+            <button className={button}><i className="fa-solid fa-hand-holding-heart"></i><p className="hidden xl:flex">Grok</p></button>
+            <button className={button}><i className="fa-regular fa-bookmark"></i><p className="hidden xl:flex">Yer İşaretleri</p></button>
+            <button className={button}><i className="fa-solid fa-briefcase"></i><p className="hidden xl:flex">İşler</p></button>
+            <button className={button}><i className="fa-solid fa-user-group"></i><p className="hidden xl:flex">Topluluklar</p></button>
+            <button className={button}><i className="fa-regular fa-circle-check"></i><p className="hidden xl:flex">Premium</p></button>
+            <button className={button}><i className="fa-solid fa-poo-storm"></i><p className="hidden xl:flex">Onaylı Kuruluşlar</p></button>
+            <button className={button}><i className="fa-regular fa-user"></i><p className="hidden xl:flex">Profil</p></button>
+            <button className={button}><i className="fa-solid fa-ellipsis"></i><p className="hidden xl:flex">Daha Fazla</p></button>
+            <button className="bg-white text-black flex items-center justify-center gap-5 rounded-full w-full h-12 text-xl font-medium"><p className="hidden xl:flex">Gönderi Yayınla</p></button>
             </div>
         </ul>
         </div>
